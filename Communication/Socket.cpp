3acc9a19602a2d9aa94a74ff62466d6b7d2049cd
frawni <caroline@forestcall.be>
2019-02-08 14:46:00 +0100

    [Mod] bits and pieces of a socket

--- Communication/Socket.cpp
+++ Communication/Socket.cpp
@@ -1,10 +1,26 @@
-#include "config.hpp"
 #include "Socket.hpp"
 
 Socket::Socket() {
   file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+  my_addr.sin_family = AF_INET;
+  my_addr.sin_port = htons(MYPORT);
+  my_addr.sin_addr.s_addr = INADDR_ANY;
+  memset(&(my_addr.sin_zero), '\0', 8);
+}
+
+Socket::Socket(std::string ip_addr) {
+  Socket();
+  my_addr.sin_addr.s_addr = inet_addr(ip_addr);
 }
 
 Socket::~Socket() {
   close(file_descriptor);
+}
+
+int Socket::getFileDescriptor() {
+  return file_descriptor;
+}
+
+sockaddr_in Socket::getAddress() {
+  return my_addr;
 }
--- Communication/Socket.hpp
+++ Communication/Socket.hpp
@@ -1,8 +1,21 @@
+#include "config.hpp"
+
+#ifndef  _SOCK_H_
+#define  _SOCK_H_
 
 class Socket {
 private:
   int file_descriptor;
+  sockaddr_in my_addr;
 
+public:
   Socket();
+  Socket(std::string ip_addr);
   ~Socket();
+
+  int getFileDescriptor();
+  sockaddr_in getAddress();
+
 };
+
+#endif
