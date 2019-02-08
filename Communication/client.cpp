eb1a2bb770908686b585f73b9ba197c45433a52c
Duc <Duc.Nguyen@ulb.ac.be>
2019-02-08 14:05:36 +0100

    Communication

--- Communication/client.cpp
+++ Communication/client.cpp
@@ -1,13 +1,12 @@
 #include "config.hpp"
 
-using namespace std;
 
 int main(){
   // Initialisation socket
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   if (sockfd == -1){
-    cerr << "[Error] Socket" << std::endl;
+    std::cerr << "[Error] Socket" << std::endl;
     return 1;
   }
 
@@ -15,16 +14,17 @@ int main(){
   their_addr.sin_family = AF_INET;
   their_addr.sin_port = htons(MYPORT);
   their_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Addresse IP a remplacer
+  memset(&(their_addr.sin_zero), '\0', 8);
 
   // Demande de connexion au serveur
   int connectRes = connect(sockfd, (sockaddr*)&their_addr, sizeof(their_addr));
 
   if (connectRes == -1){
-    cerr << "[Error] Connect" << std::endl;
+    std::cerr << "[Error] Connect" << std::endl;
     return 1;
   }
   else {
-    cout << "Connected from " << sockfd << endl;
+    std::cout << "Connected from " << sockfd << std::endl;
   }
 
   while (true) {
--- Communication/server.cpp
+++ Communication/server.cpp
@@ -1,14 +1,12 @@
 #include "config.hpp"
-#include <cstring>
 
-using namespace std;
 
 int main(){
   // Initialisation socket serveur
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   if (sockfd == -1){
-    cerr << "[Error] Socket" << std::endl;
+    std::cerr << "[Error] Socket" << std::endl;
     return 1;
   }
 
@@ -22,19 +20,19 @@ int main(){
   int yes = 1;
 
   if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-  	perror("[Error] Setsockopt");
+  	std::cerr << "[Error] Setsockopt" << std::endl;
     exit(1);
   }
 
   // Lier socket au port et a l'adresse
   if (bind(sockfd, (struct sockaddr*)&my_addr, sizeof(struct sockaddr)) == -1){
-    cerr << "[Error] Bind" << std::endl;
+    std::cerr << "[Error] Bind" << std::endl;
     return 1;
   }
 
   // Met le socket serveur en attente de connexions
   if (listen(sockfd, BACKLOG) == -1){
-    cerr << "[Error] Listen" << std::endl;
+    std::cerr << "[Error] Listen" << std::endl;
   }
 
   while (true){
@@ -46,17 +44,17 @@ int main(){
     int new_fd = accept(sockfd, (struct sockaddr*)&their_addr, &their_size);
 
     if (new_fd == -1){
-      cerr << "[Error] Accept" << std::endl;
+      std::cerr << "[Error] Accept" << std::endl;
       continue;
     }
     else {
-      cout << "Successfully connected" << endl;
+      std::cout << "Successfully connected" << std::endl;
     }
 
     // Fork et traite la demande du client
     if (!fork()){
       std::cout << "Connexion de " << inet_ntoa(their_addr.sin_addr) << std::endl;
-      cout << "Connected to " << new_fd << endl;
+      std::cout << "Connected to " << new_fd << std::endl;
       // Operation serveur
     }
     close(new_fd);
