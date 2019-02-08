eb7052bc448ec0588fe1eb12b10cb992dc378dfb
Duc <Duc.Nguyen@ulb.ac.be>
2019-02-08 14:45:00 +0100

    Replace for loop by while loop

--- Communication/parser.cpp
+++ Communication/parser.cpp
@@ -4,7 +4,7 @@
 
 int main(){
   // Blanc puis noir et piece = char, pos1, pos2
-  std::string msg = "pa2ka4r5dqc9Ke1bf8";
+  std::string msg = "pa2ka4r5dqc9Ke1bf8!";
 
   // Dictionnaire clé : Symbole et valeur = Nom piece
   std::map<char, std::string> pieceName;
@@ -16,10 +16,13 @@ int main(){
   pieceName['K'] = "King";
 
 
-  for (unsigned int a = 0; a < msg.length(); a += 3){
+  unsigned int a = 0;
+
+  while (msg[a] != '!'){
     std::cout << "Piece : " << pieceName[msg[a]] << std::endl;
     std::cout << "Position : " << msg[a+1] << "-" << msg[a+2] << std::endl;
     std::cout << std::endl;
+    a += 3;
   }
 
 
