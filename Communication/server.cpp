2ac022d60b42b44927509c620d96510e41124315
Duc <Duc.Nguyen@ulb.ac.be>
2019-02-09 13:50:04 +0100

    Queue + thread

--- Communication/server.cpp
+++ Communication/server.cpp
@@ -1,5 +1,14 @@
 #include "config.hpp"
 #include "BindSocket.hpp"
+#include <queue>
+#include <thread>
+
+void startParty(int fd1, int fd2){
+  // Lance une partie
+  std::cout << "La partie commence" << std::endl;
+  std::cout << "Joueur 1 " << fd1 << std::endl;
+  std::cout << "Joueur 2 " << fd2 << std::endl;
+}
 
 
 int main(){
@@ -15,15 +24,25 @@ int main(){
   binding_socket.activate();
 
   while (true){
+    std::queue<int> players; // Vecteur filedescriptors des joueurs connectes
     sockaddr_in their_addr;
-    Socket talking_socket = binding_socket.createSocket(their_addr);
+    Socket client_socket = binding_socket.createSocket(their_addr);
+    players.push(client_socket.getFileDescriptor());
 
-    // Fork et traite la demande du client
-    if (!fork()){
-      std::cout << "Connexion de " << inet_ntoa(their_addr.sin_addr) << std::endl;
-      std::cout << "Connected to " << talking_socket.getFileDescriptor() << std::endl;
-      // Operation serveur
+    // Gere la demande du client avec des threads
+    std::cout << "Connexion de " << inet_ntoa(their_addr.sin_addr) << std::endl;
+    std::cout << "Connected to " << client_socket.getFileDescriptor() << std::endl;
+
+    // Lance une partie avec les deux premiers joueurs arrives
+    if (players.size() >= 2){
+      int player1 = players.front();
+      players.pop();
+      int player2 = players.front();
+      players.pop();
+      std::thread partyThread(startParty, player1, player2);
     }
+
+    // Operation serveur
   }
 
   return 0;
