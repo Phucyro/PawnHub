971ec5841c3ef56530e41b6adf02865ff829c035
mhuwart <mhuwart@gmail.com>
2019-02-09 19:16:32 +0000

    [fix] small optimization + warnings Board.cpp

--- Code/Board.cpp
+++ Code/Board.cpp
@@ -10,11 +10,11 @@ Board::Board(unsigned column, unsigned row):
 	_row(row),
 	_state(nullptr)
 {
-	_state = new Piece**[this->getColumn()];
-	for (int i = 0; i < this->getColumn(); i++)
+	_state = new Piece**[this->_column];
+	for (unsigned i = 0; i < this->_column; i++)
 	{
-		_state[i] = new Piece*[this->getRow()];
-		for (int j = 0; j < this->getRow(); j++)
+		_state[i] = new Piece*[this->_row];
+		for (unsigned j = 0; j < this->_row; j++)
 		{
 			_state[i][j] = nullptr;
 		}
@@ -24,8 +24,8 @@ Board::Board(unsigned column, unsigned row):
 
 //copy
 Board::Board(const Board& original):
-	_column(original.getColumn()),
-	_row(original.getRow()),
+	_column(original._column),
+	_row(original._row),
 	_state(nullptr)
 {
 	this->_copyState(original);
@@ -33,9 +33,9 @@ Board::Board(const Board& original):
 
 
 //transfert
-Board::Board(Board&& original):
-	_column(original.getColumn()),
-	_row(original.getRow()),
+constexpr Board::Board(Board&& original):
+	_column(original._column),
+	_row(original._row),
 	_state(original._state)
 {
 	original._state = nullptr;
@@ -54,8 +54,8 @@ Board::~Board()
 //=copy
 Board& Board::operator= (const Board& original)
 {
-	_column = original.getColumn();
-	_row = original.getRow();
+	_column = original._column;
+	_row = original._row;
 	this->_delState();
 	this->_copyState(original);
 	return *this;
@@ -65,8 +65,8 @@ Board& Board::operator= (const Board& original)
 //=transfert
 Board& Board::operator= (Board&& original)
 {
-	_column = original.getColumn();
-	_row = original.getRow();
+	_column = original._column;
+	_row = original._row;
 	_state = original._state;
 	original._state = nullptr;
 	return *this;
@@ -82,11 +82,11 @@ precondition: the original board have the same size than this one.
 postcondition: this board have the same state than original.
 */
 {
-	_state = new Piece**[this->getColumn()];
-	for (int i = 0; i < this->getColumn(); i++)
+	_state = new Piece**[this->_column];
+	for (unsigned i = 0; i < this->_column; i++)
 	{
-		_state[i] = new Piece*[this->getRow()];
-		for (int j = 0; j < this->getRow(); j++)
+		_state[i] = new Piece*[this->_row];
+		for (unsigned j = 0; j < this->_row; j++)
 		{
 			_state[i][j] = original._state[i][j];//getPiece? TODO
 		}
@@ -100,7 +100,7 @@ Free the place taken by _state and change it to nullptr
 postcondition: _state == nullptr
 */
 {
-	for (int i; i < this->getColumn(); i++)
+	for (int i; i < this->_column; i++)
 	{
 		delete[] _state[i];
 	}
@@ -111,7 +111,7 @@ postcondition: _state == nullptr
 
 
 
-Piece* Board::getCase(Coordinate place)
+Piece* Board::getCase(Coordinate place) const
 {
 	return _state[place.getRealColumn()][place.getRealRow()];
 }
