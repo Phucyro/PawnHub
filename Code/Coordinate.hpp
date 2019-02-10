c83000915261071cd3bcd4363f7dc79727bfdd2e
mhuwart <mhuwart@gmail.com>
2019-02-10 11:40:23 +0000

    optimization Coordinate.hpp

--- Code/Coordinate.hpp
+++ Code/Coordinate.hpp
@@ -8,15 +8,15 @@ class Coordinate
 	unsigned _row;
 	
 	public:
-	Coordinate(unsigned, unsigned);
-	Coordinate(int, int);
-	Coordinate(char, char);
-	Coordinate(const Coordinate&) = default;
+	constexpr Coordinate(unsigned column, unsigned row) noexcept : _column(column), _row(row) {}
+	constexpr Coordinate(int column, int row) noexcept : Coordinate(unsigned(column), unsigned(row)) {}
+	constexpr Coordinate(char column, char row) noexcept : _column(column - 'A'), _row(row - '1') {}
+	constexpr Coordinate(const Coordinate&) noexcept = default;
 	~Coordinate() = default;
 	
 	Coordinate& operator= (const Coordinate&) = default;
-	bool operator== (const Coordinate&);
-	bool operator!= (const Coordinate&);
+	bool operator== (const Coordinate&) const;
+	bool operator!= (const Coordinate&) const;
 	
 	unsigned getRealColumn() const {return _column;}
 	unsigned getRealRow() const {return _row;}
