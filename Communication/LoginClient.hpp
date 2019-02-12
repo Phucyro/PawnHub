f8a7c34c4dc705b2d5e84c45ff60ecf297d2e097
Duc Trung Nguyen <duc.nguyen@ulb.ac.be>
2019-02-12 17:10:31 +0000

    Update

--- Communication/LoginClient.hpp
+++ Communication/LoginClient.hpp
@@ -5,12 +5,13 @@
 #include "Socket.hpp"
 
 
-void login(Socket* socket, std::string username, std::string password){
+bool login(Socket* socket, std::string username, std::string password){
   /*
   Identification du joueur pour lui permettre de joueur
   Socket socket : socket du joueur qui veut se connecter
   string username : nom de compte du joueur
   string password : mot de passe du compte du joueur
+  Renvoie true si l'utilisateur a reussi a s'identifier
   */
 
   // Envoie du type de service 0 : Identification
@@ -26,23 +27,26 @@ void login(Socket* socket, std::string username, std::string password){
   switch (answer){
     case '0' :
       std::cout << "Vous venez de vous connecter" << std::endl;
-      break;
+      return true;
     case '1' :
       std::cout << "Compte utilisateur inexistant" << std::endl;
       break;
     case '2' :
       std::cout << "Mauvais mot de passe" << std::endl;
   }
+
+  return false;
 }
 
 
-void createAccount(Socket* socket, std::string username, std::string pswd1, std::string pswd2){
+bool createAccount(Socket* socket, std::string username, std::string pswd1, std::string pswd2){
   /*
   Permet de joueur au joueur de s'inscrire et donc d'avoir un compte
   Socket socket : socket du joueur qui veut se connecter
   string username : nom de compte du joueur
   string pswd1 : mot de passe du compte du joueur
   string pswd2 : mot de passe du compte du joueur (confirmation)
+  Renvoie true si le compte a ete cree correctement
   */
 
   if (pswd1 != pswd2){
@@ -62,34 +66,41 @@ void createAccount(Socket* socket, std::string username, std::string pswd1, std:
       case '0' :
         std::cout << "Compte cree avec succes" <<  std::endl;
         login(socket, username, pswd1);
-        break;
+        return true;
       case '1' :
         std::cout << "Nom de compte existant" << std::endl;
-        break;
     }
   }
+
+  return false;
 }
 
 void connectRegister(Socket* socket){
-  bool connected = false;
+  /*
+  Demande a l'utilisateur s'il veut se connecter ou s'enregistrer et traite sa
+  demande.
+  Socket* socket : pointer vers le socket de l'utilisateurs
+  */
+
+  bool user_connected = false;
   int service;
   std::string username;
   std::string password1;
   std::string password2;
 
-  //while (!connected){
+  while (!user_connected){
     std::cout << "[Option] 0) Connexion | 1) Inscription : ";
     std::cin >> service;
 
     switch (service){
       case 0 : // Demande de connection
-        std::cout << "Username :" << std::endl;
+        std::cout << "Username : ";
         std::cin >> username;
-        std::cout << "Password :" << std::endl;
+        std::cout << "Password : ";
         std::cin >> password1;
-        login(socket, username, password1);
+        if (login(socket, username, password1))
+          user_connected = true;
         break;
-
       case 1 : // Demande de creation d'un compte
         std::cout << "Username : ";
         std::cin >> username;
@@ -97,24 +108,11 @@ void connectRegister(Socket* socket){
         std::cin >> password1;
         std::cout << "Password (confirmation) : ";
         std::cin >> password2;
-        createAccount(socket, username, password1, password2);
+        if (createAccount(socket, username, password1, password2))
+          user_connected = true;
         break;
     }
-  //}
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
