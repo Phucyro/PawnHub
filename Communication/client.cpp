51063382520cf6048a92196e5a4e921451c0f73c
Duc Trung Nguyen <duc.nguyen@ulb.ac.be>
2019-02-13 15:35:08 +0000

    Update

--- Communication/client.cpp
+++ Communication/client.cpp
@@ -1,7 +1,7 @@
 #include "config.hpp"
 #include "Socket.hpp"
 #include "LoginClient.hpp"
-// #include "StartPartyClient.hpp"
+//#include "../Display/board.cpp"
 
 
 int main(){
@@ -12,7 +12,14 @@ int main(){
   socket.connectToServer("127.0.0.1");
 
   // L'utilisateur doit s'identifier ou creer un compte
-  connectRegister(&socket);
+  authentification(&socket);
+
+  /*
+  // Ouvre la fenetre (ne marche pas makefile doit changer)
+  Board board;
+  board.init_ncurses();
+  sleep(10);
+  */
 
   return 0;
 }
