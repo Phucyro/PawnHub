3ae2c5bb7024627e86bb472b03a3e6e0560d99c2
aantuness <andre.antunes.coimbra@ulb.ac.be>
2019-02-13 17:58:31 +0100

    [ADD]  - Unitary Bishop test [EDIT] - Edit Makefile        - put Pawn,Piece,Bishop outside chesspieces directory
    Co-Authored-By: mhuwart <mhuwart@gmail.com>

---
+++ Code/Piece.cpp
@@ -0,0 +1,44 @@
+#ifndef  __PIECE__CPP__
+#define __PIECE__CPP__
+
+#include "Piece.hpp"
+
+Piece& Piece::operator= (const Piece& original){
+	_color = original._color;
+	_coords = original._coords;
+	_isTaken = original._isTaken;
+	for (int i = 0; i < 3; i++){
+				_str[i] = original._str[i];
+			}
+			return *this;
+}
+
+
+Piece* Piece::_doMove(Coordinate end, Board* board){
+	Piece *takenPiece;
+	takenPiece = board->movePiece(_coords, end);
+	takenPiece->changeIsTaken();
+	return takenPiece
+}
+
+void Piece::_reverseMove(Coordinate end, Board* board, Piece* takenPiece){
+	board->movePiece(end, _coords);
+	board->setCase(end, takenPiece);
+	takenPiece->changeIsTaken(game.getTurn(), this, board);
+}
+
+bool Piece::move(Coordinate end, Board* board, Game& game){
+/*Move this piece to the correct location on the board and return true if the move is possible. Else return false and leave the board unchanged*/
+	if(!(this->_checkMove(end, board, game))) return false;
+	Piece* takenPiece = this->_doMove(end, board);
+
+	if(game.testCheck()){
+		this->_reverseMove(end, board, takenPiece);
+		return false;
+	}
+	_coords = end;
+	return true;
+}
+
+
+#endif
--- Code/Piece.hpp
+++ Code/Piece.hpp
@@ -1,2 +1,55 @@
-class Piece// just here to be able to test Board 
-{};
+#ifndef  __PIECE__HPP__
+#define __PIECE__HPP__
+
+#include "Coordinate.hpp"
+#include "Board.hpp"
+#include "Game.hpp"
+
+#define TYP 0 // type
+#define ROW 1 // row
+#define COL 2 // column
+
+
+class Piece {
+
+	protected :
+
+		char 				_color  ;		// 'w' ou 'b'
+	  Coordinate 	_coords ; 	// coords of Piece
+    bool 				_isTaken; 	// tells if piece has been taken
+		char 				_str[3]	; 	// string containing info about piece
+
+		constexpr Piece(const char color, const char column, const char row) : _color(color), _coords(column, row){
+			_str[ROW] = coords.getAbstractRow();
+			_str[COL] = coords.getAbstractColumn();
+		}
+
+		Piece(const char& color,Coordinate& coords) : _color(color),_coords(coords),_isTaken(false),_str() {
+			_str[ROW] = coords.getAbstractRow();
+			_str[COL] = coords.getAbstractColumn();
+		}
+
+		Piece(const Piece& original) : _color(original._color), _coords(original._coords), _isTaken(original._isTaken), _str() {
+			for (int i = 0; i < 4; i++){
+				_str[i] = original._str[i];
+			}
+		}
+
+
+		virtual bool _checkMove(Coordinate, Board*, Game&) = 0;
+		virtual void _doMove(Coordinate, Board*);
+		virtual void _reverseMove(Coordinate, Board*, Piece*);
+
+	public :
+
+		Piece& operator= (const Piece&);
+    virtual bool move(Coordinate, Board*, Game&);
+		virtual ~Piece() = default;
+		inline char getColor() const {return _color;}
+    virtual char* toString() {return _str;}
+		virtual void changeIsTaken(unsigned turn, Piece*, Board*){_isTaken = !_isTaken;}
+		bool isTaken() const {return _isTaken;}
+
+};
+
+#endif
