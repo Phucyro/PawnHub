24b297df42a3df5aa83a843f2b153e058db4f3e6
aantuness <andre.antunes.coimbra@ulb.ac.be>
2019-02-15 20:07:59 +0100

    [ADD] - unitary test for Queen (not complete)       - unitary test for Rook (not complete but nearly ) [EDIT]- added friend TestRook to Rook.hpp

--- Code/Rook.hpp
+++ Code/Rook.hpp
@@ -4,6 +4,8 @@
 #include "Piece.hpp"
 #include "Pawn.hpp"
 
+class TestRook;
+
 class Rook : public Piece {
 
 	protected :
@@ -11,12 +13,13 @@ class Rook : public Piece {
 		bool _checkMove(Coordinate, Board*, Game&) override;
 
 	public :
-
-		Rook(const char& color, Coordinate& coords) noexcept : Piece(color, coords), _moved(false) {
+		Rook(const char color, Coordinate coords) noexcept : Piece(color, coords), _moved(false) {
 			_str[TYP] = 'r';
 		}
-		Rook(const char color, const char column , const char row) : Piece(color,column,row), _moved(false){
 
+		Rook(const char color, const char column , const char row) : Piece(color,column,row), _moved(false){
+			_str[TYP] = 'r';
+		}
 
 		Rook(const Rook& original) noexcept : Piece(original), _moved(original.hasMoved()) {}
 		explicit Rook(const Pawn& original) noexcept : Piece(original), _moved(original.hasMoved()){
@@ -29,6 +32,9 @@ class Rook : public Piece {
 		bool hasMoved() const {return _moved;}
 		bool move(Coordinate, Board*, Game&) override;
 		bool canMove(Board*, Game&) override;
+
+		friend TestRook;
+
 };
 
 #endif
