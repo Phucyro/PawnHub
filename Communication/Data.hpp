9f9fdcd62eec3252c2248230c197e843620c2b65
Duc Trung Nguyen <duc.nguyen@ulb.ac.be>
2019-02-15 19:13:52 +0000

    Upload New File

---
+++ Communication/Data.hpp
@@ -0,0 +1,22 @@
+#ifndef _DATA_H_
+#define _DATA_H_
+
+#include <map>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include "SplitString.hpp"
+
+class Data {
+private:
+  std::map<std::string, std::string> _data;
+  std::string const _path;
+
+public:
+  Data(std::string file_path);
+  void load();
+  void write();
+  void addAccount(std::string username, std::string password);
+};
+
+#endif
