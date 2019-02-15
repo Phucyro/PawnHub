3107a660a322645fc44d58bb8addad36a0003545
Duc Trung Nguyen <duc.nguyen@ulb.ac.be>
2019-02-15 19:19:01 +0000

    Update file

--- Communication/server.cpp
+++ Communication/server.cpp
@@ -1,24 +1,26 @@
 #include "config.hpp"
 #include "BindSocket.hpp"
+#include "ServerReceiveHandler.hpp"
+#include <map>
 #include <thread>
-#include "treatConnection.hpp"
-
-// #include "startPartyServer.hpp"
+#include "Data.hpp"
 
 int main(){
-  // Initialisation socket serveur
+  Data data("database.txt"); // bug makefile
+  data.load();
+
   BindSocket binding_socket;
 
-  // Met le socket serveur en attente de connexions
+  // Met le socket rveur en attente de connexions
   binding_socket.activate();
 
   while (true){
     // Accepte l'utilisateur dans le serveur et lui asssocie un socket
-    Socket client_socket = binding_socket.createSocket();
+    Socket* client_socket = binding_socket.createSocket();
 
-    // Demande a l'identifiant de se connecter ou de s'inscrire
-    std::thread thread(treatConnection, &client_socket);
-    thread.join(); // a deplacer ou meme enlever si possible
+    // Traite la demande de connexion
+    std::thread thread(serverReceiveHandler, client_socket, &data);
+    thread.detach();
   }
 
   return 0;
