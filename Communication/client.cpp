17536781941e9259fb0612f07ed37f73af118e7c
Duc Trung Nguyen <duc.nguyen@ulb.ac.be>
2019-02-16 15:10:57 +0000

    Update structure

--- Communication/client.cpp
+++ Communication/client.cpp
@@ -10,15 +10,12 @@ int main(){
   // Demande de connexion au serveur
   socket.connectToServer("127.0.0.1");
 
-  // L'utilisateur doit s'identifier ou creer un compte
-  //authentification(&socket);
-
   // Thread gere messages recus
-  std::thread receive_thread(clientReceiveHandler, &socket);
+  std::thread receive_thread(receiveMessageHandler, &socket);
   receive_thread.detach();
 
   // Gere messages envoyes envoye par le client
-  clientSendHandler(&socket);
+  sendMessageHandler(&socket);
 
   return 0;
 }
