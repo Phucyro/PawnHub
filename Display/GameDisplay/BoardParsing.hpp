f480c1ac52194d73df71b04e167b77c83c7a8919
Cyro <hpham@ulb.ac.be>
2019-03-03 19:02:57 +0100

    [add] print alice's pieces

--- Display/GameDisplay/BoardParsing.hpp
+++ Display/GameDisplay/BoardParsing.hpp
@@ -58,4 +58,36 @@ void stringToBoard(std::string message) {
   }
 }
 
+void separateAlicePieces(unsigned a,std::string message,std::string colour) {
+  const char piece = pieceMap[message[a]];
+  Coordinate coor(message[a+1], message[a+2]);
+  int column = coor.getRealColumn();
+  int line = coor.getRealRow();
+  if (colour == "white") {
+    attron(COLOR_PAIR(WHITE_PLAYER));
+    mvprintw(1+(line)*OFFSET, 54+((column+1)*OFFSET), "%c", piece);
+    attroff(COLOR_PAIR(WHITE_PLAYER));
+  }
+  else {
+    attron(COLOR_PAIR(BLACK_PLAYER));
+    mvprintw(1+(line*OFFSET), 54+((column+1)*OFFSET), "%c", piece);
+    attroff(COLOR_PAIR(BLACK_PLAYER));
+  }
+}
+
+void aliceToBoard(std::string message) {
+  unsigned int a = 0;
+  std::string colour = "white";
+  while (message[a] != '!'){
+    separateAlicePieces(a, message, colour);
+    a += CHAR_NUM;
+  }
+  a += 1;
+  colour = "black";
+  while (message[a] != '#'){
+    separateAlicePieces(a, message, colour);
+    a += CHAR_NUM;
+  }
+}
+
 #endif
